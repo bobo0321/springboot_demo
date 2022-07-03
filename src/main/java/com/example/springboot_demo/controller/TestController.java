bb2375e3c293922ca980241f4b7a0747180dc9df
范波 <bo633270@gmail.com>
2022-07-03 20:14:00 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot_demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.springboot_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fan/test")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello";
+    }
+}
